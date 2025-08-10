74c7651232997557f978ebee9584cb35813b0113
Matheus <mathe.system@gmail.com>
2025-08-10 18:09:07 -0300

    test: ensure CreateSnippet Use Case calls repo correctly

---
+++ api/src/modules/snippets/domain/use-cases/test/create-snippet.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { mock, MockProxy } from 'vitest-mock-extended'
+
+import { ISnippetsRepo } from '@/modules/snippets/domain/protocols/repos/snippets-repo.interface'
+import { CreateSnippetUC } from '@/modules/snippets/domain/use-cases/create-snippet'
+import { mockSnippet } from '@/modules/snippets/infra/db/models/test/mock/snippet.mock'
+
+describe('CreateSnippetUC', () => {
+  let sut: CreateSnippetUC
+  let repo: MockProxy<ISnippetsRepo>
+  let mockedSnippet = mockSnippet()
+
+  beforeEach(() => {
+    repo = mock<ISnippetsRepo>()
+    mockedSnippet = mockSnippet()
+    repo.create.mockResolvedValue(mockedSnippet)
+
+    sut = new CreateSnippetUC(repo)
+  })
+
+  const mockParams = () => ({ text: 'any_text' })
+
+  it('should call repo with correct params', async () => {
+    const params = mockParams()
+
+    await sut.perform(params)
+
+    expect(repo.create).toHaveBeenCalledTimes(1)
+    expect(repo.create).toHaveBeenCalledWith(params)
+  })
+})
