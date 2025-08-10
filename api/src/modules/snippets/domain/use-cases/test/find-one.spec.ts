7d2557aa0186ade5c0fb270a852eebe4b1d55ad4
Matheus <mathe.system@gmail.com>
2025-08-10 19:09:37 -0300

    test: ensure FindOneSnippetUC calls repo with correct params and also returns correct values on success

---
+++ api/src/modules/snippets/domain/use-cases/test/find-one.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { mock, MockProxy } from 'vitest-mock-extended'
+
+import { ISnippetsRepo } from '@/modules/snippets/domain/protocols/repos/snippets-repo.interface'
+import { FindOneSnippetUC } from '@/modules/snippets/domain/use-cases/find-one-snippet'
+import { mockSnippet } from '@/modules/snippets/infra/db/models/test/mock/snippet.mock'
+
+describe('FindOneSnippetUC', () => {
+  let sut: FindOneSnippetUC
+  let repo: MockProxy<ISnippetsRepo>
+  let mockedSnippet = mockSnippet()
+
+  beforeEach(() => {
+    repo = mock<ISnippetsRepo>()
+    mockedSnippet = mockSnippet()
+    repo.findOne.mockResolvedValue(mockedSnippet)
+
+    sut = new FindOneSnippetUC(repo)
+  })
+
+  const mockParams = (): string => 'any_valid_id'
+
+  it('should call repo with correct params', async () => {
+    const params = mockParams()
+
+    await sut.perform(params)
+
+    expect(repo.findOne).toHaveBeenCalledTimes(1)
+    expect(repo.findOne).toHaveBeenCalledWith(params)
+  })
+
+  it('should return correct values on success', async () => {
+    const newSnippet = await sut.perform(mockParams())
+
+    expect(newSnippet).toEqual(mockedSnippet)
+  })
+})
